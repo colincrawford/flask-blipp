a1c479a6ffe7a25a41c2c42ddb2e93e8c0ea4233
colinlcrawford <ccolin84@gmail.com>
2019-10-01 20:23:11 -0400

    refactor to use an input formatter for customization

--- flask_blipp/flask_blipp.py
+++ flask_blipp/flask_blipp.py
@@ -1,18 +1,37 @@
 import sys
 
 
-def flask_blipp(app, stdout=sys.stdout, ignore_head=True, ignore_options=True):
+def flask_blipp(app, stdout=sys.stdout, formatter=None, ignored_http_methods=None):
     """
     Prints out the routes for a flask app on start up
 
     app - a flask app
     stdout - where to print the routes to
     """
-    rules = [rule for rule in app.url_map.iter_rules()]
-    for rule in rules:
-        for method in rule.methods:
-            if method == "OPTIONS" and ignore_options:
-                continue
-            if method == "HEAD" and ignore_head:
-                continue
-            stdout.write(f"{method} {rule}\n")
+    if not ignored_http_methods:
+        ignored_http_methods = ["OPTIONS", "HEAD"]
+    ignored_methods = set(ignored_http_methods)
+
+    if not formatter:
+        formatter = basic_formatter
+
+    rules = [
+        (rule, filter_methods(rule.methods, ignored_methods))
+        for rule in app.url_map.iter_rules()
+    ]
+
+    stdout.write(formatter(rules))
+
+
+def filter_methods(methods, ignored_methods):
+    return [method for method in methods if method not in ignored_methods]
+
+
+def basic_formatter(routes):
+    output = []
+
+    for (rule, methods) in routes:
+        output.extend([f"{method} {rule}" for method in methods])
+
+    output.append("\n")
+    return "\n".join(output)
